bc49d9fd37a1a8c9e493e2d7196c6558eeb4bb0d
juliussediolano <julius.sediolano@asurion.com>
2021-10-18 21:00:06 +0900

    complete terraform;

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,10 +1,14 @@
 locals {
 
+  count = 2
+
   ami_id = "ami-0df99b3a8349462c6"
 
   key_pair = "jp-devops-asurion-poc-apne1-keypair"
 
-  my_ip_cidr = ["154.21.216.228"]
+  security_group_ids = ["sg-61d44018"]
+
+  subnet_id = ""
 
   tags = {
     BUSINESS_UNIT="PSS"
--- terraform/main.tf
+++ terraform/main.tf
@@ -1,26 +1,6 @@
 
-
-
-resource "aws_security_group_rule" "ingress" {
-  type              = "ingress"
-  from_port         = 0
-  to_port           = 65535
-  protocol          = "tcp"
-  cidr_blocks       = [aws_vpc.example.cidr_block]
-  ipv6_cidr_blocks  = [aws_vpc.example.ipv6_cidr_block]
-  security_group_id = "sg-123456"
-}
-
-resource "aws_security_group_rule" "egress" {
-  type              = "egress"
-  from_port         = 0
-  to_port           = 0
-  protocol          = -1
-  ipv6_cidr_blocks  = local.my_ip_cidr
-  security_group_id = "sg-123456"
-}
-
 resource "aws_instance" "kubernetes" {
+
   count         = local.count
   
   ami           = local.ami_id
@@ -29,5 +9,7 @@ resource "aws_instance" "kubernetes" {
 
   tags          = local.tags
 
-  vpc_security_group_ids = []
+  vpc_security_group_ids = local.security_group_ids
+
+  subnet_id = local.subnet_id
 }
